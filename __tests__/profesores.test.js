f29738f059b27119673951da3798aef4704e9263
Emmanuel Chable <emmanuelisai_28@hotmail.com>
2023-03-28 21:28:27 -0600

    added route and test for create profesor endpoint

--- __tests__/profesores.test.js
+++ __tests__/profesores.test.js
@@ -3,41 +3,90 @@ const axios = require('axios');
 // Create mock
 jest.mock('axios');
 
-describe("Testing endpoints", () => {
-    test("GET /profesores", async () => {
-        const fake = [
-            {
-              "id": 1,
-              "nombres": "Emmanuel Isai",
-              "apellidoPaterno": "Chable",
-              "apellidoMatenerno": "Colli",
-              "numeroEmpleado": 1,
-              "horasClase": 10,
-              "createdAt": Date(),
-              "updatedAt": Date()
-            },  {
-                "id": 2,
-                "nombres": "Juan",
-                "apellidoPaterno": "Ramón",
-                "apellidoMatenerno": "Gonzales",
-                "numeroEmpleado": 2,
-                "horasClase": 5,
-                "createdAt": Date(),
-                "updatedAt": Date()
-              }];
+describe('Testing endpoints', () => {
+  test('GET /profesores', async () => {
+    const fake = [
+      {
+        id: 1,
+        nombres: 'Emmanuel Isai',
+        apellidoPaterno: 'Chable',
+        apellidoMatenerno: 'Colli',
+        numeroEmpleado: 1,
+        horasClase: 10,
+        createdAt: Date(),
+        updatedAt: Date(),
+      },
+      {
+        id: 2,
+        nombres: 'Juan',
+        apellidoPaterno: 'Ramón',
+        apellidoMatenerno: 'Gonzales',
+        numeroEmpleado: 2,
+        horasClase: 5,
+        createdAt: Date(),
+        updatedAt: Date(),
+      },
+    ];
 
-        // Set result
-        axios.get.mockResolvedValue(fake);
+    // Set result
+    axios.get.mockResolvedValue(fake);
 
-        // Get result
-        const result =  await axios.get('http://localhost:3000/api/v1/profesores');
-        
-        // Test called get
-        const axiosSpy = jest.spyOn(axios, 'get');
-        expect(axiosSpy).toHaveBeenCalledTimes(1);
-        
-        // Test result
-        expect(result).toEqual(fake);
+    // Get result
+    const result = await axios.get('http://localhost:3000/api/v1/profesores');
 
-    })
+    // Test called get
+    const axiosSpy = jest.spyOn(axios, 'get');
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+
+    // Test result
+    expect(result).toEqual(fake);
+  });
+  test('POST /profesores', async () => {
+    const tokenValue =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtbWFudWVsaXNhaV8yOEBob3RtYWlsLmNvbSIsInBhc3N3b3JkIjoicGFzc3dvcmQyOC4ifQ.GgWS-JYKUfoBEnJadeNa24970hksD1whwoU4_qBFgSY';
+    axios.post.mockResolvedValue(tokenValue);
+
+    const loginData = {
+      email: 'emmanuelisai_28@hotmail.com',
+      password: 'password28.',
+    };
+    const token = await axios.post(
+      'http://localhost:3000/api/v1/login',
+      loginData
+    );
+
+    // Test called get
+    const axiosSpy = jest.spyOn(axios, 'post');
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(tokenValue).toEqual(token);
+
+    const fake = {
+      id: 1,
+      nombres: 'Emmanuel Isai',
+      apellidoPaterno: 'Chable',
+      apellidoMatenerno: 'Colli',
+      numeroEmpleado: 1,
+      horasClase: 10,
+      createdAt: Date(),
+      updatedAt: Date(),
+    };
+
+    // Set result
+    axios.post.mockResolvedValue(fake);
+
+    // Get result
+    const result = await axios.post(
+      'http://localhost:3000/api/v1/profesores',
+      fake,
+      {
+        Authorization: 'Bearer {{JWT}}',
+      }
+    );
+
+    // Test called get
+    expect(axiosSpy).toHaveBeenCalledTimes(2);
+
+    // Test result
+    expect(result).toEqual(fake);
+  });
 });
--- controllers/profesorController.js
+++ controllers/profesorController.js
@@ -1,13 +1,17 @@
 const Profesor = require('../models/profesor');
 
 const getAll = async (req, res) => {
-    const profesores = await Profesor.findAll();
-    return res.status(200).json(profesores);    
-}
+  const profesores = await Profesor.findAll();
+  return res.status(200).json(profesores);
+};
 
-
+const create = async (req, res) => {
+  const body = req.body;
+  const profesor = await Profesor.create(body);
+  return res.status(201).json(profesor);
+};
 
 module.exports = {
-    getAll
-}
-
+  getAll,
+  create,
+};
