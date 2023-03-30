5b64e8cfa2330169ebfc6cbbadae492d7b1ef006
VictorWars <cauichdavalosvictorenrique@gmail.com>
2023-03-30 00:28:05 -0600

    Modulos de Alumnos
    Desarrollo de la función de create() en el controlador.
    Desarrollo de la ruta de POST /alumnos en el archivo de ruta.
    Implementar pruebas unitarias para el endpoint.
    
    Desarrollo de la función de edit() en el controlador.
    Desarrollo de la ruta de PUT /alumnos en el archivo de ruta.
    Implementar pruebas unitarias para el endpoint.
    
    Desarrollo de la función de getById() en el controlador.
    Desarrollo de la ruta de GET /alumnos/:alumnoId en el archivo de ruta.
    Implementar pruebas unitarias para el endpoint.
    
    Desarrollo de la función de destroy() en el controlador.
    Desarrollo de la ruta de DELETE /alumnos/:alumnoId en el archivo de ruta.
    Implementar pruebas unitarias para el endpoint.

--- __tests__/alumnos.test.js
+++ __tests__/alumnos.test.js
@@ -1,43 +1,173 @@
-const axios = require('axios');
+const axios = require("axios");
 
 // Create mock
-jest.mock('axios');
+jest.mock("axios");
+let token = "";
 
 describe("Testing endpoints", () => {
-    test("GET /alumnos", async () => {
-        const fake = [
-            {
-              "id": 1,
-              "nombres": "Victor Enrique",
-              "apellidoPaterno": "Cauich",
-              "apellidoMaterno": "Davalos",
-              "matricula": 15002051,
-              "promedio": 7.2,
-              "createdAt": Date(),
-              "updatedAt": Date()
-            },  {
-                "id": 2,
-                "nombres": "Javier Ramón",
-                "apellidoPaterno": "Torres",
-                "apellidoMaterno": "Peniche",
-                "matricula": 15004844,
-                "promedio": 7.2,
-                "createdAt": Date(),
-                "updatedAt": Date()
-              }];
+  test("Login", async () => {
+    const tokenValue =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtbWFudWVsaXNhaV8yOEBob3RtYWlsLmNvbSIsInBhc3N3b3JkIjoicGFzc3dvcmQyOC4ifQ.GgWS-JYKUfoBEnJadeNa24970hksD1whwoU4_qBFgSY";
+    axios.post.mockResolvedValue(tokenValue);
 
-        // Set result
-        axios.get.mockResolvedValue(fake);
+    const loginData = {
+      email: "emmanuelisai_28@hotmail.com",
+      password: "password28.",
+    };
+    const tokenResult = await axios.post(
+      "http://localhost:3000/api/v1/login",
+      loginData
+    );
+    const axiosSpy = jest.spyOn(axios, "post");
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(tokenValue).toEqual(tokenResult);
+    token = tokenResult;
+  });
 
-        // Get result
-        const result =  await axios.get('http://localhost:3000/api/v1/alumnos');
-        
-        // Test called get
-        const axiosSpy = jest.spyOn(axios, 'get');
-        expect(axiosSpy).toHaveBeenCalledTimes(1);
-        
-        // Test result
-        expect(result).toEqual(fake);
+  test("GET /alumnos", async () => {
+    const fake = [
+      {
+        id: 1,
+        nombres: "Victor Enrique",
+        apellidoPaterno: "Cauich",
+        apellidoMaterno: "Davalos",
+        matricula: 15002051,
+        promedio: 7.2,
+        createdAt: Date(),
+        updatedAt: Date(),
+      },
+      {
+        id: 2,
+        nombres: "Javier Ramón",
+        apellidoPaterno: "Torres",
+        apellidoMaterno: "Peniche",
+        matricula: 15004844,
+        promedio: 7.2,
+        createdAt: Date(),
+        updatedAt: Date(),
+      },
+    ];
 
-    })
+    // Set result
+    axios.get.mockResolvedValue(fake);
+
+    // Get result
+    const result = await axios.get("http://localhost:3000/api/v1/alumnos");
+
+    // Test called get
+    const axiosSpy = jest.spyOn(axios, "get");
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+
+    // Test result
+    expect(result).toEqual(fake);
+  });
+
+  test("POST /alumnos", async () => {
+    const fake = {
+      id: 1,
+      nombres: "Victor Enrique",
+      apellidoPaterno: "Cauich",
+      apellidoMaterno: "Davalos",
+      matricula: 15002051,
+      promedio: 7.2,
+      createdAt: Date(),
+      updatedAt: Date(),
+    };
+    // Set result
+    axios.post.mockResolvedValue(fake);
+
+    // Get result
+    const result = await axios.post(
+      "http://localhost:3000/api/v1/alumnos",
+      fake,
+      {
+        Authorization: `Bearer ${token}`,
+      }
+    );
+    const axiosSpy = jest.spyOn(axios, "post");
+
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(fake);
+  });
+
+  test("PUT /alumnos/:id", async () => {
+    const fake = {
+      id: 1,
+      nombres: "Victor Enrique",
+      apellidoPaterno: "Cauich",
+      apellidoMaterno: "Davalos",
+      matricula: 15002066,
+      promedio: 10,
+      createdAt: Date(),
+      updatedAt: Date(),
+    };
+
+    axios.put.mockResolvedValue(fake);
+
+    // Get result
+    const result = await axios.put(
+      "http://localhost:3000/api/v1/alumnos",
+      {
+        nombres: "Victor Enrique",
+      },
+      {
+        Authorization: `Bearer ${token}`,
+      }
+    );
+    const axiosSpy = jest.spyOn(axios, "put");
+
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(fake);
+  });
+
+  test("GET /alumnos/:id", async () => {
+    const fake = {
+      id: 1,
+      nombres: "Victor Enrique",
+      apellidoPaterno: "Cauich",
+      apellidoMaterno: "Davalos",
+      matricula: 15002066,
+      promedio: 10,
+      createdAt: Date(),
+      updatedAt: Date(),
+    };
+
+    axios.get.mockResolvedValue(fake);
+
+    // Get result
+    const result = await axios.get("http://localhost:3000/api/v1/alumnos/1");
+    const axiosSpy = jest.spyOn(axios, "get");
+
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(fake);
+  });
+
+  test("DELETE /alumnos/:id", async () => {
+    const fake = {
+      id: 1,
+      nombres: "Victor Enrique",
+      apellidoPaterno: "Cauich",
+      apellidoMaterno: "Davalos",
+      matricula: 15002066,
+      promedio: 10,
+      createdAt: Date(),
+      updatedAt: Date(),
+    };
+
+    axios.delete.mockResolvedValue(fake);
+
+    // Get result
+    const result = await axios.delete(
+      "http://localhost:3000/api/v1/alumnos/1",
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const axiosSpy = jest.spyOn(axios, "delete");
+
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(fake);
+  });
 });
--- routes/alumnoRoutes.js
+++ routes/alumnoRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const alumnoController = require('../controllers/alumnoController');
+const authentication = require('../middlewares/authentication');
 const router = express.Router();
 
 router.get('/alumnos', alumnoController.getAll);
+router.put('/alumnos/:id', authentication, alumnoController.update);
+router.post('/alumnos', authentication, alumnoController.create);
+router.get('/alumnos/:id', authentication, alumnoController.getById);
+router.delete('/alumnos/:id', authentication, alumnoController.destroy);
 
 module.exports = router;
