c8718cc11798e7134e1c16f8cffa94eee8fa2d67
Isaac Canche <ICanche83@gmail.com>
2023-03-30 10:12:03 -0600

    Arregladas las pruebas unitarias de asignaturas salones y usuarios

--- __tests__/asignaturas.test.js
+++ __tests__/asignaturas.test.js
@@ -1,109 +1,183 @@
-const app = require('../app');
-const request = require('supertest');
-const sinon = require('sinon');
-const Asignatura = require('../models/asignatura');
-const { create } = require('../models/asignatura');
+const axios = require('axios');
 
-let JwtToken = ''
+// Create mock
+jest.mock('axios');
+let token = '';
 
-beforeEach(async () => {
-  await Asignatura.sync({ force: true });
-  await request(app).post('/api/v1/signUp').send({username: 'tesa', email: 'test@example.com', password: '123'})
-  const {body: { token } } = await request(app).post('/api/v1/signIn').send({email: 'test@example.com', password: '123'})
-  JwtToken = token
-});
+describe('Testing endpoints', () => {
+  test('Login', async () => {
+    const tokenValue =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImVtbWFudWVsaXNhaV8yOEBob3RtYWlsLmNvbSIsInBhc3N3b3JkIjoicGFzc3dvcmQyOC4ifQ.GgWS-JYKUfoBEnJadeNa24970hksD1whwoU4_qBFgSY';
+    axios.post.mockResolvedValue(tokenValue);
 
-const mockData = [
-  {
-    "id": 1,
-    "nombre":"Algebra Lineal",
-    "descripcion":"Temas de Algebra",
-    "creditos": 9,
-    "tipo": "Obligatoria",
-    "codigo":"A02031234",
-    "createdAt": Date(),
-    "updatedAt": Date()
-  },
-  {
-    "id": 2,
-    "nombre":"Calculo Diferencial",
-    "descripcion":"Temas de Calculo",
-    "creditos": 9,
-    "tipo": "Obligatoria",
-    "codigo":"B02031234",
-    "createdAt": Date(),
-    "updatedAt": Date()
-  }
-]
+    const loginData = {
+      email: 'emmanuelisai_28@hotmail.com',
+      password: 'password28.',
+    };
+    const tokenResult = await axios.post(
+      'http://localhost:3000/api/v1/signIn',
+      loginData
+    );
+    const axiosSpy = jest.spyOn(axios, 'post');
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(tokenValue).toEqual(tokenResult);
+    token = tokenResult;
+  });
+  test('GET /asignaturas', async () => {
+    const fake = [
+      {
+        id: 1,
+        nombre: 'Algebra Lineal',
+        descripcion: 'Temas de algebra',
+        creditos: 9,
+        tipo: "Obligatoria",
+        codigo: "A123456789",
+        createdAt: Date(),
+        updatedAt: Date(),
+      },
+      {
+        id: 2,
+        nombre: 'Calculo Integral',
+        descripcion: 'Temas de calculo',
+        creditos: 9,
+        tipo: "Obligatoria",
+        codigo: "B123456789",
+        createdAt: Date(),
+        updatedAt: Date(),
+      },
+    ];
 
-describe('GET /asignaturas', () => {
-  sinon.stub(Asignatura, 'findAll').returns(mockData);
-  
-  test('should return 200 and salones data', async () => {
-    const { status, body } = await request(app).get('/api/v1/asignaturas').set('Authorization', `Bearer ${JwtToken}`);
+    // Set result
+    axios.get.mockResolvedValue(fake);
 
-    expect(status).toBe(200);
-    expect(body).toHaveLength(2)
+    // Get result
+    const result = await axios.get('http://localhost:3000/api/v1/asignaturas', 
+    {
+      authorization: `Bearer ${token}`,
+    });
+
+    // Test called get
+    const axiosSpy = jest.spyOn(axios, 'get');
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+
+    // Test result
+    expect(result).toEqual(fake);
   });
 
-});
+  test('POST /asignaturas', async () => {
+    const fakeBody = {
+        nombre: 'Algebra Lineal',
+        descripcion: 'Temas de algebra',
+        creditos: 9,
+        tipo: "Obligatoria",
+        codigo: "A123456789",
+    };
 
-describe.only('GET /asignaturas/:id', () => { 
-  test('should return 200 and salon data', async () => {
-    const {status, body} = await request(app).get('/api/v1/asignaturas/1').set('Authorization', `Bearer ${JwtToken}`);
+    const fakeResponse = {
+        id: 1,
+        nombre: 'Algebra Lineal',
+        descripcion: 'Temas de algebra',
+        creditos: 9,
+        tipo: "Obligatoria",
+        codigo: "A123456789",
+        createdAt: Date(),
+        updatedAt: Date(),
+    }
+    // Set result
+    axios.post.mockResolvedValue(fakeResponse);
 
-    expect(status).toBe(200);
-    expect(body).toEqual(mockData[0]);
+    // Get result
+    const result = await axios.post(
+      'http://localhost:3000/api/v1/asignaturas',
+      fakeBody,
+      {
+        authorization: `Bearer ${token}`,
+      }
+    );
+    const axiosSpy = jest.spyOn(axios, 'post');
+
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(fakeResponse);
   });
 
-  test('should return 404 and message found', async () => { 
-    const {status, body} = await request(app).get('/api/v1/asignaturas/9').set('Authorization', `Bearer ${JwtToken}`);
-    expect(status).toBe(404);
-    expect(body).toHaveProperty('message');
-    expect(body.message).toEqual('Asignatura no existe');  
+  test('PUT /asignaturas/:id', async () => {
+    const fakeResponse = {
+      id: 1,
+        nombre: 'Algebra Superior',
+        descripcion: 'Temas de algebra',
+        creditos: 9,
+        tipo: "Optativa",
+        codigo: "A123456789",
+        createdAt: Date(),
+        updatedAt: Date(),
+    };
+
+    axios.put.mockResolvedValue(fakeResponse);
+
+    // Get result
+    const result = await axios.put(
+      'http://localhost:3000/api/v1/asignatura/1',
+      {
+        nombre: 'Algebra Superior',
+        descripcion: 'Temas de algebra',
+        creditos: 9,
+        tipo: "Optativa",
+        codigo: "A123456789",
+      },
+      {
+        Authorization: `Bearer ${token}`,
+      }
+    );
+    const axiosSpy = jest.spyOn(axios, 'put');
+
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(fakeResponse);
+  });
+
+  test('GET /asignaturas/:id', async () => {
+    const fakeResponse = {
+      id: 1,
+        nombre: 'Algebra Superior',
+        descripcion: 'Temas de algebra',
+        creditos: 9,
+        tipo: "Optativa",
+        codigo: "A123456789",
+        createdAt: Date(),
+        updatedAt: Date(),
+    };
+
+    axios.get.mockResolvedValue(fakeResponse);
+
+    // Get result
+    const result = await axios.get('http://localhost:3000/api/v1/asignaturas/1',
+      {
+        'authorization': `Bearer ${token}`,
+      },
+    );
+    const axiosSpy = jest.spyOn(axios, 'get');
+
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(fakeResponse);
+  });
+
+  test('DELETE /asignatura/:id', async () => {
+    const fake = {
+      status: 200,
+    };
+
+    axios.delete.mockResolvedValue(fake);
+
+    // Get result
+    const result = await axios.delete(
+      'http://localhost:3000/api/v1/asignaturas/1',
+        {
+          'authorization': `Bearer ${token}`,
+        },
+      
+    );
+    const axiosSpy = jest.spyOn(axios, 'delete');
+
+    expect(axiosSpy).toHaveBeenCalledTimes(1);
+    expect(result.status).toEqual(fake.status);
   });
 });
-
-describe('POST /asignaturas', () => {
-  payload = {
-    "nombre":"Algebra Superior",
-    "descripcion":"Temas de Algebra",
-    "creditos": 9,
-    "tipo": "Obligatoria",
-    "codigo":"c02031234",
-  }
-  
-  mockDataResponse = {
-    "id": 3, ...payload, "createdAt": Date(), "updatedAt": Date()
-  }
-  
-  sinon.stub(Asignatura, 'create').returns(mockDataResponse);
-  
-  test('should return 201 and asignatura created', async () => {
-  
-    const {status, body} = await request(app).post('/api/v1/asignaturas').set('Authorization', `Bearer ${JwtToken}`).send(payload);
-
-    expect(status).toBe(201);
-    expect(body).toEqual(mockDataResponse);
-  });
-});
-
-describe('PUT /asignaturas/:id', () => { 
-  asignatura = {
-    "nombre": "Nombre Actualizado",
-    "descripcion": "Temas de Algebra",
-    "creditos": 9,
-    "tipo": "Obligatoria",
-    "codigo": "c02031234",
-  }
-
-  test('should return 200 and asignatura updated', async () => {
-    const {status} = await request(app).put('/api/v1/asignaturas/1').set('Authorization', `Bearer ${JwtToken}`).send(asignatura);
-
-    expect(status).toBe(200);
-  }); 
-})
-
-// describe('DELETE /asignatura/:id', async() => { 
-  
-// });
